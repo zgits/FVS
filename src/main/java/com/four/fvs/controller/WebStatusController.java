f8b04180acff12c33b3775acc262aed47bd06be3
jorksz <1223360222@qq.com>
2019-06-03 15:51:01 +0800

    历史界面简单完成

--- src/main/java/com/four/fvs/controller/WebStatusController.java
+++ src/main/java/com/four/fvs/controller/WebStatusController.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -28,6 +27,13 @@ public class WebStatusController {
     @Autowired
     private WebStatusService webStatusService;
 
+
+    /**
+     * 接收数据
+     *
+     * @param webStatus 网站实体
+     * @return
+     */
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
     public Result<Object> add(@RequestBody WebStatus webStatus){
@@ -38,12 +44,21 @@ public class WebStatusController {
         return ResultUtils.paramerror();
     }
 
+
+    /**
+     * 根据时间和type进行查询
+     * type为1：每周访问量的查询
+     * type为2：当日时间段的查询
+     * type为3：每周前10播放量视频查询
+     *
+     * @param startTime 开始时间
+     * @param type  查询分类
+     * @return
+     */
     @RequestMapping(value = "{startTime}/{type}" ,method = RequestMethod.GET)
     @ResponseBody
     public Result<Object> query(@PathVariable("startTime") @DateTimeFormat(pattern = "yyyy-MM-dd HH:ss:mm") Date startTime,
                                     @PathVariable("type") Integer type){
-        System.out.println("type is :"+type);
-        System.out.println("time is :"+startTime);
         if(type == 1){
             int result = webStatusService.countVisitForWeek(startTime);
             return result >= 0 ? ResultUtils.success(result) : ResultUtils.serviceerror();
